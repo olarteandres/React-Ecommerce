bf0a13e31cbbfbc89c81c03a2f8f7363feda349e
olarteandres <andresolarte31@gmail.com>
2022-10-05 14:57:29 -0300

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve( __dirname, "build" ),
     filename: 'bundle.[contenthash].js',
-    publicPath: '/',
+    publicPath: './',
     clean: true,
   },
   resolve: {
